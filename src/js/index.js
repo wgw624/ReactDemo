f3a6d06c4e84427941abc6193f038f00541190ec
wgw <406569565@qq.com>
2018-06-03 22:49:34 +0800

    helloworld

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 
 class Welcome extends React.Component{
   render(){
-    return "<h1> Hello World!...!</h1>"
+    return (<h1> Hello World!...!</h1>)
   }
 }
+
+ReactDOM.render(
+  <Welcome />,
+  document.getElementById('root')
+);
