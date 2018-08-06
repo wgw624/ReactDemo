081efda276d6054763b7e914877c7d5fe164e1fe
weiguangwei <274555306@qq.com>
2018-08-06 10:10:14 +0800

    样式

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 export default class ComponentHeader extends React.Component{
   render(){
+    var styleCompentCss={
+      head:{
+        backgroundColor:'#cccccc',
+        color:'#ffffff'
+      }
+    }
     return(
-      <header>
+      <header style={styleCompentCss.head} className='header'>
         <h1>CarsOA 系统</h1>
       </header>
     )
